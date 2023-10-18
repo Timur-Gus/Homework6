08ce7502ee37b59dbab8f44b6223a953797f0908
Timur-Gus <timurka24478@gmail.ru>
2023-10-18 11:50:00 +0300

    Задача3

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,12 @@ public class Main {
         System.out.print("Числа от 10 до 1: ");
         for (int i = 10; i >= 1; i--)
             System.out.print(i + " ");
+
+        System.out.println("\n");
+        System.out.println("Задача 3");
+        System.out.println();
+        System.out.print("Четные числа от 0 до 17: ");
+        for (int i = 0; i <= 17; i += 2)
+            if(i % 2 == 0 && i != 0) System.out.print(i + " ");
     }
 }
