fc5ba036502805eec9a008a921ad24921756f3d4
Timur-Gus <timurka24478@gmail.ru>
2023-10-18 12:02:00 +0300

    Задача6

--- src/Main.java
+++ src/Main.java
@@ -30,5 +30,10 @@ public class Main {
         System.out.println();
         for (int i = 1904; i <= 2096; i += 4)
             System.out.println(i + " год является високосным");
+        System.out.println();
+        System.out.println("Задача 6");
+        System.out.println();
+        for (int i = 7; i <= 98; i += 7)
+            System.out.print(i + " ");
     }
 }
