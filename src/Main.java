adef086eb09e5e411b4de560979e6af62678a4e1
Timur-Gus <timurka24478@gmail.ru>
2023-10-18 11:38:03 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Задача 1");
+        System.out.println();
+        System.out.print("Числа от 1 до 10: ");
+        for (int i = 1; i <= 10; i++)
+            System.out.print(i + " ");
+    }
+}
